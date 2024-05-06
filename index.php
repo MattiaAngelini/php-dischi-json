ce69add4bac4ce5d439e5d802a77c33f63865796
MattiaAngelini <mattiaangelini1993@gmail.com>
2024-05-06 15:12:28 +0200

    Init Project

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!--CSS-->
+    <link rel="stylesheet" href="css/style.css">
+    <!--BOOTSTRAP-->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <!--AXIOS-->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/axios/0.27.2/axios.min.js" integrity="sha512-odNmoc1XJy5x1TMVMdC7EMs3IVdItLPlCeL5vSUPN2llYKMJ2eByTTAIiiuqLg+GdNr9hF6z81p27DArRFKT7A==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+    <!--VUE-->
+    <script src="https://unpkg.com/vue@3"></script>
+    <title>php-dischi-json</title>
+</head>
+<body>
+    
+    <header> </header>
+    
+    <!--CARDS-->
+    <main> 
+        <section class="container d-flex justify-content-evenly flex-wrap">
+
+            <div class="ms-card m-4"></div>
+
+        </section>
+    </main>
+
+<script src="js/script.js"></script>
+</body>
+</html>
